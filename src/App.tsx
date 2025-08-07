fa455b9fa95964c1c3a1d14533c3275ecd553251
torimugi <zoo.zoo.0903@outlook.jp>
2025-08-07 18:53:12 +0900

    カウントボタンを実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+const [ count, setCount ] = useState(0)
+
   return (
     <>
-
+<div>
+  <p>{count}</p>
+  <button onClick={() => setCount(count + 1)} >count</button>
+</div>
     </>
-  )
+  
+
+)
 }
 
 export default App
