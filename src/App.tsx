a595f5b84f2ed9e76d2bd8e2b5b50c166daa3926
torimugi <zoo.zoo.0903@outlook.jp>
2025-08-20 17:44:49 +0900

    テストデータを一覧で表示する

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,22 @@ import './App.css'
 
 function App() {
 
+  const recordsrecords = [
+    { title: "勉強の記録1", time: 1},
+    { title: "勉強の記録2", time: 3},
+    { title: "勉強の記録3", time: 5}
+]
+
   return (
     <>
 <h1>学習記録一覧</h1>
-    </>
+{recordsrecords.map((record) => {
+return(
+  <p>{record.title}</p>
+)
+})}
+</>
+
 )
 }
-
-export default App
+export default App;
