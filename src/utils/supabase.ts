8fbe311cace13c1373e28b12d7c367b4d0ceb295
torimugi <zoo.zoo.0903@outlook.jp>
2025-09-26 18:42:04 +0900

    supabaseクライアントを修正

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,6 +1,8 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = "https://uxacvctolqjuvqsctkov.supabase.co"; 
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV4YWN2Y3RvbHFqdXZxc2N0a292Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg0NTg1MjgsImV4cCI6MjA3NDAzNDUyOH0.evjII6BVr9FLeNirzp6n92sm_uAi1Y3ee9A6XrQswuA";
+const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!, 
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export default supabase;
