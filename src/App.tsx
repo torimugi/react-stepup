5353b1a7ae04998b8dcaf48e18deb62eac77b41a
torimugi <zoo.zoo.0903@outlook.jp>
2026-03-22 22:51:52 +0900

    Github Actions の設定を変更して再プッシュ

--- src/App.tsx
+++ src/App.tsx
@@ -89,7 +89,7 @@ error();
 }
 , []);
 if (isLoading) {
-  return <div>Loading....</div>; // ローディング状態であればLoading...を表示
+  return <div>Loading...</div>; // ローディング状態であればLoading...を表示
 }
 
 const totalStudyTime = records.reduce((total, record) => {
